9478be8b85c22a92c6867859f1abd9f3b34d325e
TechLeadMyancare <136549701+TechLeadMyancare@users.noreply.github.com>
2023-10-03 10:40:41 +0630

    first commit

---
+++ app/src/main/java/com/mtz/zinkoko/NumberListActivity.kt
@@ -0,0 +1,12 @@
+package com.mtz.zinkoko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtz.ptta.R
+
+class NumberListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_list)
+    }
+}
